df405341126fdffccdc2000be09859803c149045
Mikk Martin <mikk.martin@gmail.com>
2022-06-10 14:19:28 +0300

    some cleanup

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export default async function main() {
   const activeTabUrl = await runAppleScript(
     `tell application "Google Chrome" to get URL of active tab of first window`
   );
-  // const localIP = await runAppleScript(`ifconfig | grep 'inet.*broadcast' | awk '{print $2}'`);
   if (activeTabUrl.startsWith("http://localhost")) {
     const localIP = await runAppleScript(`IPv4 address of (get system info)`);
     const localUrl = activeTabUrl.replace("http://localhost", `http://${localIP}`);
