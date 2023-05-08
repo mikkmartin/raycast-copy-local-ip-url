8f3956c2435586a890f130273ee280a5fc523f2a
Mikk Martin <mikk.martin@gmail.com>
2023-05-08 14:17:48 +0300

    use default browser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { showHUD, Clipboard } from "@raycast/api";
+import { Clipboard, getDefaultApplication, showHUD } from "@raycast/api";
+import { readFile, writeFile } from "fs/promises";
+import { resolve } from "path";
 import { runAppleScript } from "run-applescript";
 
 export default async function main() {
+  let file = await readFile(resolve(__dirname, "test.html")).catch(() => undefined);
+  if (!file) await writeFile(resolve(__dirname, "test.html"), "");
+  const { name } = await getDefaultApplication(resolve(__dirname, "test.html"));
+
   const activeTabUrl = await runAppleScript(
-    `tell application "Google Chrome" to get URL of active tab of first window`
+    `tell application "${name}" to get URL of active tab of first window`
   );
   if (activeTabUrl.startsWith("http://localhost")) {
     const localIP = await runAppleScript(`IPv4 address of (get system info)`);
